e2387b086d97cabf899014389f625dc84a96794a
Theo-allaeys <theo.allaeys@student.odisee.be>
2022-10-02 18:44:20 +0200

    vak add

--- webapp/js/script.js
+++ webapp/js/script.js
@@ -4,14 +4,18 @@
 	/*jslint devel: true*/
 	let baseApiAddress = "https://theoallaeys2021.be/web&mobile/taak1/api/";
 	const fromstudent = document.getElementById("addstudent");
+	const formvak = document.getElementById("addvak");
 	const add_voornaam = fromstudent.elements['add_voornaam'];
 	const add_achternaam = fromstudent.elements['add_achternaam'];
 	const add_studentennummer = fromstudent.elements['add_studentennummer'];
 	const add_cursus = fromstudent.elements['add_cursus'];
+	const add_code = formvak.elements['add_code'];
+	const add_naam = formvak.elements['add_naam'];
+	const add_taal = formvak.elements['add_taal'];	
 	/* Vorige lijn aanpassen naar de locatie op jouw domein! */
 
 	let alertEl = document.getElementById("alert");
-	let options = document.getElementById('cursusoption');
+	let options = document.getElementById("cursusoption");
 	let opties = {
 		method: "POST", // *GET, POST, PUT, DELETE, etc.
 		mode: "cors", // no-cors, *cors, same-origin
@@ -152,16 +156,47 @@
 
 		if (data.length > 0) {
 			// er zit minstens 1 item in list, we geven dit ook onmiddelijk weer
-			var tLijst = "";
-			for (var i = 0; i < data.length; i++) {
+			let tLijst = " ";
+			for (let i = 0; i < data.length; i++) {
 				tLijst += " <option value=" + data[i].code + ">" + data[i].naam + "</option>";
 			}
-			optioner(tLijst);
+
+			add_cursus.innerHTML = tLijst;
 		} else {
 			optioner("Servertijd kon niet opgevraagd worden");
 		}
 
 	}
+
+	function addvak() {
+        // een ONVEILIGE manier om gebruikersgegevens te testen
+
+        let url = baseApiAddress + "vakadd.php";
+        // onze php api verwacht een paar parameters
+        // we voegen deze toe aan de body van de opties
+
+        // body data type must match "Content-Type" header
+        opties.body = JSON.stringify({
+            vak_code: add_code.value,
+            vak_naam: add_naam.value,
+            vak_taal: add_taal.value,
+            format: "json"
+        });
+
+        // test de api
+        fetch(url, opties)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function() {
+                //correct toegevoegd?
+                getApivak();
+            })
+            .catch(function(error) {
+                // verwerk de fout
+                alertEl.innerHTML = "fout : " + error;
+            });
+    }
 
 	// EventListeners
 	document.getElementById("btnGetProducten").addEventListener("click", function () {
@@ -170,16 +205,17 @@
 	fromstudent.addEventListener('submit', function() {
 		addStudent();
 		addStudentvak();
-	} )
+	})
+
+	formvak.addEventListener('submit', function(){
+		addvak();
+	})
 
 	window.onload =  getApivak();
 
 	// helper functies
 	function alerter(message) {
 		alertEl.innerHTML = message;
-	}
-	function optioner(message) {
-		options.innerHTML = message;
 	}
 })();
 
